76a96af8fe15388ab3bb1f2a94d80e0f6962f5f4
yomatters <yomatters@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-05-14 19:03:52 +0000

    Updated feed form tracking plugin for changes in collective.googleanalytics.

--- collective/simplesocial/analytics/feedform.py
+++ collective/simplesocial/analytics/feedform.py
@@ -1,17 +1,9 @@
 from collective.googleanalytics.tracking import AnalyticsBaseTrackingPlugin
-import os
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 
 class FeedFormTracking(AnalyticsBaseTrackingPlugin):
     """
     A tracking plugin to track submission of the feed form.
     """
     
-    def __call__(self):
-        """
-        Renders the tracking plugin.
-        """
-        
-        template_file = os.path.join(os.path.dirname(__file__), 'feedform.tpl')
-        return self.render_file(template_file, {
-            'relative_url': self.relative_url(),
-        })
+    __call__ = ViewPageTemplateFile('feedform.pt')
--- collective/simplesocial/analytics/feedform.tpl
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-    var feedform_callback = function (response) {
-        if (response != 'null') {
-            _gaq.push(["_trackEvent", "Facebook", feedform_title, "${relative_url}"]);                 
-        }
-    };
-</script>
