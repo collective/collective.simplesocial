1e5aee2fc36df5ff77db790b4d154e6c881118c0
yomatters <yomatters@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-07-23 23:48:59 +0000

    Replaced fan box with the new like box.

--- collective/simplesocial/fanbox/facebookfanbox.py
+++ collective/simplesocial/fanbox/facebookfanbox.py
@@ -5,7 +5,6 @@ from plone.portlets.interfaces import IPortletDataProvider
 
 from zope import schema
 from zope.formlib import form
-from zope.schema.vocabulary import SimpleVocabulary
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from collective.simplesocial import simplesocialMessageFactory as _
@@ -19,38 +18,36 @@ class IFacebookFanBox(IPortletDataProvider):
     """
 
     profile_id = schema.TextLine(
-        title = _(u'Profile ID'),
-        description = _(u'Paste the ID of the Facebook Page that should be promoted.  You can find it at the end of the URL of the Page when viewing it in Facebook.  (Only Pages are supported; not Groups.)'),
+        title = _(u'Facebook Page ID'),
+        description = _(u'Enter the page ID. You can copy it from'
+            u' the end of the URL of the page on Facebook.'),
         )
-    
-    stream = schema.Choice(
-        title = _(u'Display stories in activity stream?'),
-        vocabulary = SimpleVocabulary.fromItems((
-            (_(u'yes'), 1),
-            (_(u'no'), 0)
-            )),
-        default = 1,
+        
+    width = schema.Int(
+        title = _(u'Width'),
+        description = _(u'Enter the width of the box in pixels.'),
+        min = 200,
+        default = 200,
         )
-    
+        
     connections = schema.Int(
         title = _(u'Connections'),
-        description = _(u'The number of fans to display in the Fan Box. Specifying 0 hides the list of fans in the Fan Box. You cannot display more than 100 fans.'),
+        description = _(u'Show this many faces of users who have liked this page.'),
         min = 0,
         max = 100,
         default = 10,
         )
-    
-    width = schema.Int(
-        title = _(u'Width'),
-        description = _(u'Enter the desired width of the box, in pixels.'),
-        min = 200,
-        default = 200,
+        
+    show_stream = schema.Bool(
+        title = _(u'Display stream'),
+        description = _(u'Displays the public activity stream for the page.'),
+        default = True,
         )
     
-    height = schema.Int(
-        title = _(u'Height'),
-        description = _(u'Enter the desired height of the box, in pixels.'),
-        default = 554,
+    show_header = schema.Bool(
+        title = _(u'Display header'),
+        description = _(u'Displays the header "Find us on Facebook."'),
+        default = True,
         )
 
 class Assignment(base.Assignment):
@@ -62,19 +59,19 @@ class Assignment(base.Assignment):
 
     implements(IFacebookFanBox)
 
-    def __init__(self, profile_id, stream, connections, width, height):
+    def __init__(self, profile_id, width, connections, show_stream, show_header):
         self.profile_id = profile_id
-        self.stream = stream
-        self.connections = connections
         self.width = width
-        self.height = height
+        self.connections = connections
+        self.show_stream = show_stream
+        self.show_header = show_header
 
     @property
     def title(self):
         """This property is used to give the title of the portlet in the
         "manage portlets" screen.
         """
-        return "Facebook Fan Box"
+        return "Facebook Like Box"
 
 
 class Renderer(base.Renderer):
