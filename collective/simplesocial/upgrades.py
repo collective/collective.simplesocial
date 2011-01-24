51d9d7778338015a89de79f9bc242f9f7289403c
yomatters <yomatters@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-01-24 16:49:42 +0000

    Added upgrade step to 1.5.

--- collective/simplesocial/upgrades.py
+++ collective/simplesocial/upgrades.py
@@ -1,8 +1,10 @@
+from zope.app.component.hooks import getSite
 from zope.component import getUtility
 from Products.CMFCore.utils import getToolByName
 from Products.statusmessages.interfaces import IStatusMessage
 from plone.portlets.interfaces import IPortletType
 from collective.simplesocial import simplesocialMessageFactory as _
+from collective.simplesocial.browser.interfaces import IFacebookSettings
 
 def null_upgrade_step(setup_tool):
     """
@@ -15,7 +17,7 @@ def fbconnect_10_to_14(setup_tool):
     Updates the installed profiles to reflect new configuration defaults.
     """
         
-    profile_id = 'profile-collective.simplesocial:fbconnect'
+    profile_id = 'profile-collective.simplesocial:fbconnect_10_14'
     setup_tool.runImportStepFromProfile(profile_id, 'controlpanel')
     setup_tool.runImportStepFromProfile(profile_id, 'jsregistry')
     setup_tool.runImportStepFromProfile(profile_id, 'propertiestool')
@@ -54,3 +56,24 @@ def fanpage_post_10_to_14(setup_tool):
     jsregistry.renameResource(old_id, new_id)
     if '++resource++simplesocial.js' in jsregistry.getResourceIds():
         jsregistry.moveResourceAfter(new_id, '++resource++simplesocial.js')
+        
+def fbconnect_14_to_15(setup_tool):
+    """
+    Updates the installed profiles to reflect new configuration defaults.
+    """
+    
+    # See if the fanpage_post profile was installed.
+    fanpage_post_enable = False
+    jsregistry = getToolByName(setup_tool, 'portal_javascripts')
+    fanpage_post_id = '++resource++simplesocial_fanpage_post.js'
+    if fanpage_post_id in jsregistry.getResourceIds():
+        fanpage_post_enable = True
+        
+    # Import new settings from the upgrade profile.
+    profile_id = 'profile-collective.simplesocial:fbconnect_14_15'
+    setup_tool.runAllImportStepsFromProfile(profile_id)
+
+    # Toggle settings based on what was installed before.
+    settings = IFacebookSettings(getSite())
+    settings.post_to_page_available = fanpage_post_enable
+    settings.like_button_available = False
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.5dev'
+version = '1.5'
 
 long_description = (
     read('README.txt')
